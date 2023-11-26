f28e99587ddd25ab5b438945e26b797a03c98740
GianpiColonna <gcolonna@expediagroup.com>
2023-11-26 13:57:41 +0000

    Streamline find neighbor method

--- expandGridV3.py
+++ expandGridV3.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from hexalattice.hexalattice import *
 
+
 class HexGrid:
     def __init__(self, size):
         self.size = size
@@ -12,28 +13,36 @@ class HexGrid:
         # Generate hexagon centers
         return create_hex_grid(nx=self.size, ny=self.size, do_plot=False)
 
+    def find_neighbour(self, x, y, dx, dy):
+        # Check if a neighbour exists at a specified offset
+        neighbour_x, neighbour_y = x + dx, y + dy
+
+        # Use np.isclose to check if the hexagon at these coordinates exists in the grid
+        # It compares each element of the hex_centers with the neighbour_x and neighbour_y within a tolerance
+        mask = np.isclose(self.hex_centers[:, 0], neighbour_x) & np.isclose(self.hex_centers[:, 1], neighbour_y)
+
+        if np.any(mask):
+            index = np.where(mask)[0][0]
+            return [self.hex_centers[index], index]
+        return None
+
     def find_neighbours(self, hex_ix):
         # Find the neighbors of the hexagon at the specified index
         x, y = self.hex_centers[hex_ix]
 
-        neighbors = [] # List of neighbors [center, index]
+        # Directions for hexagonal grid neighbors
+        directions = [(1, 0),  # Right
+                      (-1, 0),  # Left
+                      (0.5, np.sqrt(3) / 2),  # Top right
+                      (-0.5, np.sqrt(3) / 2),  # Top left
+                      (0.5, -np.sqrt(3) / 2),  # Bottom right
+                      (-0.5, -np.sqrt(3) / 2)]  # Bottom left
 
-        if hex_ix+1 < len(self.hex_centers):                                                    # Right
-            neighbors.append([self.hex_centers[hex_ix+1], hex_ix+1])
-        if hex_ix-1 >= 0:                                                                       # Left
-            neighbors.append([self.hex_centers[hex_ix-1], hex_ix-1])
-        if y + np.sqrt(3)/2 in self.hex_centers[:, 1] and x-1/2 in self.hex_centers[:, 0]:      # Top Left
-            index = np.where(np.all(self.hex_centers == [x-1/2, y+np.sqrt(3)/2], axis=1))[0][0]
-            neighbors.append([self.hex_centers[index], index])
-        if y + np.sqrt(3)/2 in self.hex_centers[:, 1] and x+1/2 in self.hex_centers[:, 0]:      # Top Right
-            index = np.where(np.all(self.hex_centers == [x+1/2, y+np.sqrt(3)/2], axis=1))[0][0]
-            neighbors.append([self.hex_centers[index], index])
-        if y - np.sqrt(3)/2 in self.hex_centers[:, 1] and x-1/2 in self.hex_centers[:, 0]:      # Bottom Left
-            index = np.where(np.all(self.hex_centers == [x-1/2, y-np.sqrt(3)/2], axis=1))[0][0]
-            neighbors.append([self.hex_centers[index], index])
-        if y - np.sqrt(3)/2 in self.hex_centers[:, 1] and x+1/2 in self.hex_centers[:, 0]:      # Bottom Right
-            index = np.where(np.all(self.hex_centers == [x+1/2, y-np.sqrt(3)/2], axis=1))[0][0]
-            neighbors.append([self.hex_centers[index], index])
+        neighbors = []
+        for dx, dy in directions:
+            neighbour = self.find_neighbour(x, y, dx, dy)
+            if neighbour:
+                neighbors.append(neighbour)
 
         return neighbors
 
@@ -56,6 +65,7 @@ class HexGrid:
         plt.title(f'Hexagonal Grid Size {self.size}x{self.size}')
         plt.show()
 
+
 # class s-cones:
 #     def __init__(self):
 #         continue
@@ -65,5 +75,4 @@ class HexGrid:
 grid = HexGrid(size=5)
 
 grid.draw()
-neighbours = grid.find_neighbours(6)
-
+neighbours = grid.find_neighbours(24)
